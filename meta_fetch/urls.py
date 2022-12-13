a410ae8b1aaaa5c1855695e434fb4888ae4462e1
Rainemi <105657663+Rainemi@users.noreply.github.com>
2022-12-14 00:51:23 +0100

    generate and store metadata
    the updated model, views, urls, and serializers

--- meta_fetch/urls.py
+++ meta_fetch/urls.py
@@ -33,12 +33,3 @@ urlpatterns = [
    #path('ayo/', views.ayoAgba, name='ayo')
 ]
 
-
-"""
-    path("imagedata/<int:pk>/", ImageDataDetail.as_view(), name="imagedata_detail"),
-    path("imagedata/", ImageDataList.as_view(), name="imagedata_list"),
-    path("artiste/<int:pk>/", ArtisteDetail.as_view(), name="artiste_detail"),
-    path("artiste/", ArtisteList.as_view(), name="artiste_list"),
-
-    r'^tasks/(?P<pk>[0-9]+)$'
-"""
